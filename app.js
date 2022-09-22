2567337ef9227e3f4f2e60f32d16fa2700ecb6fd
Mohamed Fawaz <mohamedfawaz@Mohameds-MacBook-Pro.local>
2022-09-22 11:45:39 -0400

    fixed merge

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var usersRouter = require("./routes/users");
 var cors = require("cors");
 var moodle = require("./utils/moodle-api");
 const writeClasses = require("./utils/db-writeclass");
+const https = require('https');
+const fs = require('fs');
 
 var app = express();
 
@@ -28,6 +30,19 @@ app.use("/users", usersRouter);
 //db stuff
 db.db_connection();
 
+
+//ssl
+app.use('/', (req, res, next)=>{
+  res.send('hello from ssl')
+});
+const sslserver = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert','key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem')),
+},app);
+
+sslserver.listen(3443,()=>console.log('secure server on port 3343'));
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -38,7 +53,6 @@ app.use(async function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
